e5411dd7318f165719345c2b19660a3513c9be54
Jr_Silva_Dev <jos.pd2019@gmail.com>
2024-04-21 12:53:44 -0300

    FIX: Receiving changes

--- lib/src/app_module.dart
+++ lib/src/app_module.dart
@@ -3,7 +3,7 @@ import 'app_imports.dart';
 class AppModule extends Module {
   @override
   void routes(RouteManager r) {
-    r.module('/', module: SplasHModule());
+    r.module('/', module: SplasHModule(), transition: TransitionType.fadeIn);
     r.module('auth', module: AuthModule(), transition: TransitionType.fadeIn);
     r.module('bible_messages', module: BibleMessagesModule(), transition: TransitionType.fadeIn);
   }
