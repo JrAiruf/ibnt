e1579235de7ee85c5e948921ebffaa2fbe32355e
Jr_Silva_Dev <jos.pd2019@gmail.com>
2024-04-21 16:39:39 -0300

    FIX: Adjusting Theme

--- lib/src/app_widget.dart
+++ lib/src/app_widget.dart
@@ -6,6 +6,7 @@ class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+      theme: ThemeData(colorSchemeSeed: AppThemes.primaryColor1),
       debugShowCheckedModeBanner: false,
       routerConfig: Modular.routerConfig,
     );
