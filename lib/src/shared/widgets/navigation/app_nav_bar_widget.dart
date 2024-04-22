0d4ee34fd80162c030411d4cd436cb26fdedfa2a
Jr_Silva_Dev <jos.pd2019@gmail.com>
2024-04-22 19:25:03 -0300

    REF:Refactorin Navigation Function in Bottom Nav Bar

--- lib/src/shared/widgets/navigation/app_nav_bar_widget.dart
+++ lib/src/shared/widgets/navigation/app_nav_bar_widget.dart
@@ -1,4 +1,5 @@
-import '../../../modules/auth/auth_imports.dart';
+// ignore_for_file: must_be_immutable
+import 'package:ibnt/src/app_imports.dart';
 
 class AppNavBarWidget extends StatefulWidget {
   AppNavBarWidget({required this.pageIndex, super.key});
@@ -11,65 +12,61 @@ class AppNavBarWidget extends StatefulWidget {
 class _AppNavBarWidgetState extends State<AppNavBarWidget> {
   @override
   Widget build(BuildContext context) {
-    return AnimatedBuilder(
-      animation: PageController(initialPage: 1),
-      builder: (_, c) {
-        return BottomNavigationBar(
-          selectedItemColor: AppThemes.primaryColor1,
-          currentIndex: widget.pageIndex,
-          unselectedIconTheme: const IconThemeData(color: Colors.black),
-          selectedIconTheme: const IconThemeData(color: AppThemes.primaryColor1),
-          unselectedLabelStyle: const TextStyle(color: Colors.black),
-          selectedLabelStyle: const TextStyle(color: AppThemes.primaryColor1, fontWeight: FontWeight.w600),
-          onTap: (value) {
-            setState(() {
-              widget.pageIndex = value;
-            });
-            switch (value) {
-              case 0:
-                Modular.to.navigate('/posts/');
-                break;
-              case 1:
-                Modular.to.navigate('/home/');
-                break;
-              case 2:
-                Modular.to.navigate('/bible_messages/');
-                break;
-              case 3:
-                Modular.to.navigate('/warnings/');
-                break;
-            }
-          },
-          iconSize: 30,
-          backgroundColor: Colors.black,
-          items: const [
-            BottomNavigationBarItem(
-              label: "Postagens",
-              icon: Icon(
-                Icons.add_circle,
-              ),
-            ),
-            BottomNavigationBarItem(
-              label: "Início",
-              icon: Icon(
-                Icons.home_sharp,
-              ),
-            ),
-            BottomNavigationBarItem(
-              label: "Mensagens",
-              icon: Icon(
-                Icons.menu_book_sharp,
-              ),
-            ),
-            BottomNavigationBarItem(
-              label: "Avisos",
-              icon: Icon(
-                Icons.dataset,
-              ),
-            ),
-          ],
-        );
-      },
+    return BottomNavigationBar(
+      selectedItemColor: AppThemes.primaryColor1,
+      currentIndex: widget.pageIndex,
+      unselectedIconTheme: const IconThemeData(color: Colors.black),
+      selectedIconTheme: const IconThemeData(color: AppThemes.primaryColor1),
+      selectedLabelStyle: const TextStyle(color: AppThemes.primaryColor1, fontWeight: FontWeight.w600),
+      onTap: _setPageIndex,
+      iconSize: 30,
+      items: const [
+        BottomNavigationBarItem(
+          label: "Postagens",
+          icon: Icon(
+            Icons.add_circle,
+          ),
+        ),
+        BottomNavigationBarItem(
+          label: "Início",
+          icon: Icon(
+            Icons.home_sharp,
+          ),
+        ),
+        BottomNavigationBarItem(
+          label: "Mensagens",
+          icon: Icon(
+            Icons.menu_book_sharp,
+          ),
+        ),
+        BottomNavigationBarItem(
+          label: "Avisos",
+          icon: Icon(
+            Icons.dataset,
+          ),
+        ),
+      ],
     );
   }
+
+  _setPageIndex(int value) {
+    setState(() {
+      widget.pageIndex = value;
+    });
+    switch (value) {
+      case 0:
+        Modular.to.navigate('/posts/');
+        break;
+      case 1:
+        Modular.to.navigate('/home/');
+        break;
+      case 2:
+        Modular.to.navigate('/bible_messages/');
+        break;
+      case 3:
+        Modular.to.navigate('/warnings/');
+        break;
+    }
+  }
+
 }
