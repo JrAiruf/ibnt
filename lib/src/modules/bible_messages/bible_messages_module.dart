bd4fec0fee388c23365e50c8ecea4f90a5f1ce91
Jr_Silva_Dev <jos.pd2019@gmail.com>
2024-05-21 12:53:45 -0300

    :bug FIX: Fixing BibleMessages Page Bug

--- lib/src/modules/bible_messages/bible_messages_module.dart
+++ lib/src/modules/bible_messages/bible_messages_module.dart
@@ -8,8 +8,8 @@ class BibleMessagesModule extends Module {
   @override
   void routes(RouteManager r) {
     r.child('/', child: (_) => BlocProvider(create: (context) => Modular.get<AuthBloc>(), child: const MessagesPage()));
-    r.child('/add_messages', child: (_) => const AddMessagesPage());
-    r.child('/message', child: (_) => const MessagePage());
-    r.child('/new_message', child: (_) => const NewMessagePage());
+    r.child('/add_messages', child: (_) => BlocProvider(create: (context) => Modular.get<AuthBloc>(), child: const AddMessagesPage()));
+    r.child('/message', child: (_) => BlocProvider(create: (context) => Modular.get<AuthBloc>(), child: const MessagePage()));
+    r.child('/new_message', child: (_) => BlocProvider(create: (context) => Modular.get<AuthBloc>(), child: const NewMessagePage()));
   }
 }
--- lib/src/modules/bible_messages/view/pages/add_messages_page.dart
+++ lib/src/modules/bible_messages/view/pages/add_messages_page.dart
@@ -10,6 +10,69 @@ class AddMessagesPage extends StatefulWidget {
 class _AddMessagesPageState extends State<AddMessagesPage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    final authBloc = context.read<AuthBloc>();
+    final height = MediaQuery.sizeOf(context).height;
+    final width = MediaQuery.sizeOf(context).width;
+    final pagePadding = width * 0.035;
+    final pageCenterSpacing = height * 0.4;
+    final iconSize = height * 0.03;
+    final titleFontSize = height * 0.035;
+    final textFontSize = height * 0.017;
+    final buttonFontSize = height * 0.02;
+    final buttonPadding = height * 0.015;
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      drawer: AppDrawer(authBloc: authBloc),
+      appBar: AppBarWidget(preferredSize: Size(width, height * 0.08)),
+      body: SizedBox(
+        height: height,
+        width: width,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: pagePadding),
+          child: Column(
+            children: [
+              Align(
+                alignment: Alignment.topLeft,
+                child: Text(
+                  "Mensagens Bíblicas",
+                  style: TextStyle(
+                    fontSize: titleFontSize,
+                  ),
+                ),
+              ),
+              const Spacer(),
+              Text(
+                "Aqui você pode criar novas mensagens e abençoar os irmãos através da palavra, ou gerar uma nova mensagem bíblica automática. Após a criação, sua mensagem poderá ser compartilhada.",
+                style: TextStyle(fontSize: textFontSize, height: 0),
+              ),
+              SizedBox(height: pageCenterSpacing),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  AppButton(
+                    showBorder: true,
+                    primaryColor: AppThemes.primaryColor1,
+                    widget: Icon(Icons.email_outlined, color: AppThemes.primaryColor1, size: iconSize),
+                    fontSize: buttonFontSize,
+                    text: "Gerar Mensagem",
+                  ),
+                  SizedBox(height: buttonPadding),
+                  AppButton(
+                    primaryColor: Colors.white,
+                    backgroundColor: AppThemes.primaryColor1,
+                    widget: Icon(Icons.note_add_outlined, color: Colors.white, size: iconSize),
+                    fontSize: buttonFontSize,
+                    text: "Criar Mensagem",
+                  ),
+                ],
+              ),
+              const Spacer(),
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: AppNavBarWidget(pageIndex: 2),
+    );
   }
 }
